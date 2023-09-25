fa55904514126cc83447c377a67edb2749defe42
Agnessa <agnessa_petrova_1999@mail.ru>
2023-09-25 12:38:52 +0300

    Добавлены рестораны

--- react-20230914/src/main.jsx
+++ react-20230914/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {restaurants}  from "./constants/mock";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <div>{restaurants.map(elem=>{
+    return (<ol>
+     <li><h1>{elem.name}</h1>
+      <p>
+     {elem.menu.map}
+     {elem.reviews.map}
+      </p>
+      </li>
+    </ol>
+    )
+  })}</div>
+);
